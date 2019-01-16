3449d5ea893b44fda7c25bb8f1cb4e466f303e0f
Adelon Zeta <adelonzeta@Adelons-MacBook-Air.local>
2019-01-16 07:26:11 -0800

    use json config instead of env

--- config/Inliner.js
+++ config/Inliner.js
@@ -4,7 +4,7 @@ const siphon    = require('siphon-media-query')
 const replace   = require('gulp-replace')
 const htmlmin   = require('gulp-htmlmin')
 const fs        = require('fs')
-require('dotenv').load()
+const CONFIG    = JSON.parse(fs.readFileSync('./config.json'))
 
 function Inliner() {
   const css = fs.readFileSync('build/css/styles.css').toString()
@@ -17,8 +17,8 @@ function Inliner() {
       applyTableAttributes: true
     }))
     .pipe(replace('<!-- <style> -->', `<style>${mqCss}</style>`))
-    .pipe(replace('src="', `src="${process.env.ASSETS_URL}/`))
-    .pipe(replace('rel="icon" href="', `rel="icon" href="${process.env.ASSETS_URL}/`))
+    .pipe(replace('src="', `src="${CONFIG.assets}/`))
+    .pipe(replace('rel="icon" href="', `rel="icon" href="${CONFIG.assets}/`))
     .pipe(htmlmin({
       collapseWhitespace: true,
       conservativeCollapse: true,
