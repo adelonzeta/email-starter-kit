b0faa9c8d1fc544fff4b00ffacd756072741e766
Adelon Zeta <adelonzeta@Adelons-MacBook-Air.local>
2019-01-16 17:58:23 -0800

    offload html and text version file path to config file

--- config/Send.js
+++ config/Send.js
@@ -1,26 +1,30 @@
 const sendGrid   = require('@sendgrid/mail')
 const fs         = require('fs')
 const configFile = 'config.json'
-const htmlFile   = 'build/index.html'
-const txtFile    = 'text/index.txt'
-let CONFIG       = ''
-let HTML         = ' '
-let TXT          = ' '
+let CONFIG       = {mail: {}}
+CONFIG.mail.API_KEY   = ''
+CONFIG.mail.RECIPIENT = ''
+CONFIG.mail.SENDER    = ''
+CONFIG.mail.SUBJECT   = ''
+let TXT  = ' '
+let HTML = ' '
 
 function Send(done) {
   if (fs.existsSync(configFile))
     CONFIG = JSON.parse(fs.readFileSync(configFile))
-  if (fs.existsSync(htmlFile))
-    HTML = fs.readFileSync(htmlFile).toString()
-  if (fs.existsSync(txtFile))
-    TXT = fs.readFileSync(txtFile).toString()
-
-  let message = CONFIG.mail.message
-  message.text = TXT
-  message.html = HTML
+  if (fs.existsSync(CONFIG.mail.TEXT_VERSION))
+    TXT  = fs.readFileSync(CONFIG.mail.TEXT_VERSION).toString()
+  if (fs.existsSync(CONFIG.mail.HTML_FILE))
+    HTML = fs.readFileSync(CONFIG.mail.HTML_FILE).toString()
+  
+  let message     = {}
+  message.to      = CONFIG.mail.RECIPIENT
+  message.from    = CONFIG.mail.SENDER
+  message.subject = CONFIG.mail.SUBJECT
+  message.text    = TXT
+  message.html    = HTML
   sendGrid.setApiKey(CONFIG.mail.API_KEY)
   sendGrid.send(message)
-    .then(() => console.log('Email Sent!'))
     .catch(error => console.log(error.toString()))
   done()
 }
